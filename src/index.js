468ffe3d06ebba04ba041ce5bacd488b26ef603f
R.desing1 <r.desing4000@gmail.com>
2023-05-25 10:24:05 +0200

    fin del crud de charges todo funcionando al 100%

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 // routes
 app.use(indexRoutes);
-app.use('/api',chargesRoutes);
+app.use(chargesRoutes);
 
 // public
 app.use(express.static(path.join(__dirname,'public')));
--- src/routes/charges.routes.js
+++ src/routes/charges.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database.js');
 
+// ruta para mostrar los cargos
+
 router.get('/charges', async (req,res) =>{
     const dataCharges = await pool.query("SELECT * FROM charges");
     console.log(dataCharges);
@@ -10,21 +12,49 @@ router.get('/charges', async (req,res) =>{
     });
 });
 
+// ruta para mostrar el formulario de agg cargo
+
 router.get('/charges/add',(req,res) =>{
     res.render('charges/add.ejs');
 });
 
-
-router.post('/charges/add', async (req,res) =>{
+// ruta para guardar los cargos 
+router.post('/charges/add/', async (req,res) =>{
     const {name,description} = req.body;
     const dataCharge ={
         name:name,
         responsability:description
-    }
+    }    
     console.log(dataCharge);
     await pool.query("INSERT INTO charges SET ?",[dataCharge]);
-    res.send('cgarge successfull');
+    res.redirect('/charges');
+});    
+
+// ruta para Actualizar los cargos 
+router.post('/charges/update/', async (req,res) =>{
+    const {nameThow,descriptionThow} = req.body;
+    let textArea = recorText(descriptionThow);
+    let {idThow} = req.body;
+    const Description = {
+        name:nameThow,
+        responsability:textArea
+    }
+    await pool.query("UPDATE charges SET ? WHERE id=?",[Description,idThow]);
+    res.redirect('/charges');
+});
+
+// ruta para eliminar los cargos 
+router.get('/charges/delete/:id', async (req,res) =>{
+    const id = req.params.id;
+    await pool.query("DELETE FROM charges WHERE id=?",[id]);
+    res.redirect("/charges");
+
 });
 
 
+function recorText(textArea){
+    let textAreas = textArea;
+    let newText = textAreas.trim();
+    return newText;
+}
 module.exports = router;
