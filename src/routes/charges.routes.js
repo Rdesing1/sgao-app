c60353c509ad89f5103e4a4f969b7d2fd8efc9bd
R.desing1 <r.desing4000@gmail.com>
2023-06-19 01:55:03 +0200

    Mejorando la vista de actualizacion de charges

--- src/routes/charges.routes.js
+++ src/routes/charges.routes.js
@@ -39,23 +39,88 @@ router.post('/charges/add/',isLoggedIn, async (req, res) => {
     });
 });
 
-// ruta para Actualizar los cargos 
-router.post('/charges/update/',isLoggedIn, async (req, res) => {
-    const { nameThow, descriptionThow } = req.body;
-    let textArea = recorText(descriptionThow);
-    let { idThow } = req.body;
-    const Description = {
-        name: nameThow,
-        responsability: textArea
+router.get('/charges/update/:id', async (req,res) =>{
+    let id = req.params.id;
+    let {name,description} =  req.body;
+    objCharge = {
+        name:name,
+        responsability:description	
     }
-    await pool.query("UPDATE charges SET ? WHERE id=?", [Description, idThow]);
-    res.render("charges/index.ejs", {
-        alert: true,
-        alertTitle: "Actualizando el cargo",
-        alertHtml: "El cargo se actualizara en: ",
-        timer: 2500,
-        ruta: "/charges"
-    });
+    if(objCharge.name != 'undefined' || objCharge.responsability != 'undefined'){
+        try{
+            let charges = [];
+            const response = await pool.query('SELECT * FROM charges WHERE idcharge = ?',[id]);
+            charges = response;
+            if(charges.length > 0){
+                // res.json({data:charges[0]})
+                res.render('charges/update.ejs',{
+                    data:charges[0]
+                });
+            }else{
+                res.render("charges/index.ejs", {
+                    alertThow: true,
+                    titleDocument: `Actualizar el cargo, numero ${id}`,
+                    alertTitle: "Error",
+                    alertMessage: "Cargo no encontrado.",
+                    alertIcon: "error",
+                    showConfirmButtom: false,
+                    timer: 2500,
+                    ruta: "charges/"
+                });
+            }
+        }catch(err){
+            res.render("charges/index.ejs", {
+                alertThow: true,
+                titleDocument: `No se encontro el ${id}`,
+                alertTitle: "Error",
+                alertMessage: "Error al actualizar el cargo",
+                alertIcon: "error",
+                showConfirmButtom: false,
+                timer: 2500,
+                ruta: "charges/"
+            });
+        }
+    }else{
+        res.redirect('/charges');
+    }
+
+
+});
+
+// ruta para Actualizar los cargos 
+router.post('/charges/update/:id',isLoggedIn, async (req, res) => {
+    let id = req.params.id;
+    const {name,responsability} = req.body;
+    const Description = {
+        name,
+        responsability
+    }
+    try{
+        const result = await pool.query("UPDATE charges SET ? WHERE idcharge = ?", [Description, id]);
+        if(result.affectedRows === 1){
+            res.render("charges/index.ejs", {
+                alert: true,
+                alertTitle: "Actualizando el cargo",
+                alertHtml: "El cargo se actualizara en: ",
+                timer: 2500,
+                ruta: "/charges"
+            });
+        }else{
+            res.render("charges/index.ejs", {
+                alertThow: true,
+                titleDocument: `No se encontro el ${id}`,
+                alertTitle: "Error",
+                alertMessage: "Error al actualizar el cargo",
+                alertIcon: "error",
+                showConfirmButtom: false,
+                timer: 2500,
+                ruta: "charges/"
+            });
+        }
+        
+    }catch(err){
+        res.status(404).json({message:err});
+    }
 });
 
 // ruta para eliminar los cargos 
